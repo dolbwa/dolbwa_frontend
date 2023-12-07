6d75201462cc283c8ab84c57aa1b9de4166f3ca5
kimhongpil <idpil@kimhongpilui-MacBookPro.local>
2023-12-07 13:35:29 +0900

    fix: rewrite 옵션 제거

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  rewrites: async () => {
-    return [
-      {
-        source: '/',
-        destination: 'http://localhost:3000/',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
